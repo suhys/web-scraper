fce72bb3241ce2cf9da9b5d102d30226c7852cca
suhys <yuseung.suh@gmail.com>
2022-05-03 22:42:41 -0700

    Extracting Titles

--- indeed.py
+++ indeed.py
@@ -25,7 +25,12 @@ def extract_indeed_pages():
 
 
 def extract_indeed_jobs(last_page):
+    jobs= []
     for page in range(last_page):
         result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code)
-    
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("a", {"class":"tapItem"})
+        for result in results:
+            title = result.find("h2", {"class": "jobTitle"}).find("span", title=True).string
+            print(title)
+    return jobs

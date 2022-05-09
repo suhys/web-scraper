163dc7838c756ef154a9bb387cdaa3cba0e5cce4
suhys <yuseung.suh@gmail.com>
2022-05-08 18:18:18 -0700

    Extracting company name, location, and link

--- indeed.py
+++ indeed.py
@@ -23,14 +23,26 @@ def extract_indeed_pages():
 
     return max_page
 
+def extract_job(html):
+    title = html.find("h2", {"class": "jobTitle"}).find("span", title=True).string
+    company = html.find("span", {"class":"companyName"})
+    company_anchor = company.find("a")
+    if company_anchor is not  None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    location = html.find("div", {"class":"companyLocation"}).text
+    job_id = html["data-jk"]
+    return {'title': title,'company': company, 'location':location, 'link': f"https://www.indeed.com/viewjob?jk={job_id}"}
 
 def extract_indeed_jobs(last_page):
     jobs= []
     for page in range(last_page):
+        print(f"Scrapping page{page}")
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("a", {"class":"tapItem"})
         for result in results:
-            title = result.find("h2", {"class": "jobTitle"}).find("span", title=True).string
-            print(title)
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
--- main.py
+++ main.py
@@ -3,4 +3,6 @@ extract_indeed_jobs
 
 last_indeed_pages = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_pages)
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
+
+print(indeed_jobs)
